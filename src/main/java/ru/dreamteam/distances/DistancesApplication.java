d29fd23c20e37427c7a6cebcce0f418b73bb7d1e
niclas784 <vislobokov.aleksander@gmail.com>
2021-02-28 10:39:54 +0300

    Initial commit

---
+++ src/main/java/ru/dreamteam/distances/DistancesApplication.java
@@ -0,0 +1,13 @@
+package ru.dreamteam.distances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistancesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dreamteam/distances/DistancesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dreamteam.distances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
